53503d1c6e5d54d677fc6ae5169b88f5e14577de
tanmayrai21 <vabhavrai98@gmail.com>
2021-07-17 20:23:29 +0530

    Mobile Responsive

--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -1,16 +1,13 @@
-import classes from './MealsSummary.module.css';
+import classes from "./MealsSummary.module.css";
 
 const MealsSummary = () => {
   return (
     <section className={classes.summary}>
-      <h2>Delicious Food, Delivered To You</h2>
+      <h2>Taste is the New Trending</h2>
       <p>
-        Choose your favorite meal from our broad selection of available meals
-        and enjoy a delicious lunch or dinner at home.
-      </p>
-      <p>
-        All our meals are cooked with high-quality ingredients, just-in-time and
-        of course by experienced chefs!
+        Enjoy a delicious lunch or dinner at home. Choose your favorite item
+        from our broad selection of available meals. Cooked hygienically with
+        high-quality ingredients, just-in-time, served to you HOT and Fresh.
       </p>
     </section>
   );
